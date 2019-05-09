00f7f57310d8086d21bbf3802c2189ee6e85a5c8
Takao Kobayashi <abatanx@gmail.com>
2019-05-09 19:15:57 +0900

    Modified: LICENSE

---
+++ openg.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * openg social networking package for waggo6
+ * @copyright 2001-2011 openg project., 2019 CIEL, K.K.
+ * @license MIT
+ */
+
+global $WGCONF_AUTOLOAD;
+
+$WGCONF_AUTOLOAD[] = __DIR__ . '/include';
+$WGCONF_AUTOLOAD[] = __DIR__ . '/imc';
